b88c544ff83109f0ed4c988046abeea36644a702
Miles Hill <miles1618@gmail.com>
2018-09-16 15:29:19 -0500

    Redis dev config

--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,10 @@ module.exports = {
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://app:MyComplexPassword1234@34.207.163.153/blog',
+  redisCONFIG: {
+    port: 6379,
+    host: '35.173.131.211',
+    password: '4ea6ed6358e7d1d9c40bf62a917aae95729e90a022437a6454ed7e038bd07373'
+  },
   cookieKey: '123123123'
 };
