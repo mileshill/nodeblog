ded46bd95bcddcccbe10a4035041dee7016fa651
Miles Hill <miles1618@gmail.com>
2018-09-16 07:41:30 -0500

    initial commit

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://app:MyComplexPassword1234@34.207.163.153/blog',
   cookieKey: '123123123'
 };
