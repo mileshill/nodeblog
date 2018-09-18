3cc362aa5cf7a14927f37e435b31ae39ad75a312
Miles Hill <miles1618@gmail.com>
2018-09-18 09:46:46 -0500

    Brand logo test

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,5 +1,22 @@
-test('Adds two numbers', () => {
-    const sum = 1 + 2;
+/**
+ * Make sure to include 'await' with any command using puppeteer
+ */
 
-    expect(sum).toEqual(3);
-})
+const puppeteer = require('puppeteer');
+const APP='localhost:3000';
+
+// Launch new chromium instance
+test('Brand logo in header', async () => {
+    // Open browser
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    // Open page and navigate to app
+    const page = await browser.newPage();
+    await page.goto(APP);
+
+    // Get logo
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+    expect(text).toEqual('Blogster');
+});
