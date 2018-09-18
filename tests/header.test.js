460841d2be7ca46a6d4d640b6c915c2db0f710f6
Miles Hill <miles1618@gmail.com>
2018-09-18 10:15:42 -0500

    Oauth flow kickoff test

--- tests/header.test.js
+++ tests/header.test.js
@@ -20,10 +20,18 @@ afterEach(async ()=>{
     await browser.close();
 })
 
-
 // Launch new chromium instance
 test('Brand logo in header', async () => {
     // Get logo
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
     expect(text).toEqual('Blogster');
+});
+
+// Verify link to OAuth google flow
+test('Clicking login starts oauth flow', async () => {
+    // Clicking the 'Login with Google' anchor
+    await page.click('.right a');
+    // Look at url and verify accounts.google.com domain
+    const url = await page.url();
+    expect(url).toMatch(/accounts\.google\.com/);
 });
