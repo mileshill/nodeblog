6cc879608572cf8ca679bb26e22e1c2f8fe9248a
Miles Hill <miles1618@gmail.com>
2018-10-04 20:43:51 -0500

    Return new user object

--- tests/factories/userFactory.js
+++ tests/factories/userFactory.js
@@ -1,3 +1,8 @@
-module.exports = () => {
+// Create new user in Mongo.
+// Return userId
+const moongoose = require('moongoose');
+const User = moongoose.model('User');
 
+module.exports = () => {
+    return new User({}).save();
 };
