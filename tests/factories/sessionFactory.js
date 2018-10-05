2aa36cdf19ec12721142f7a0fb5669c7b37ad8e0
Miles Hill <miles1618@gmail.com>
2018-10-04 20:34:24 -0500

    Session factory accepts user model

--- tests/factories/sessionFactory.js
+++ tests/factories/sessionFactory.js
@@ -4,9 +4,9 @@ const keys = require('../../config/keys');
 const keygrip = new Keygrip([keys.cookieKey]);
 
 
-module.exports = () => {
+module.exports = (user) => {
     // Session Object
-    const sessionObject = {passport: {user: id}};
+    const sessionObject = {passport: {user: user._id.toString()}};
     const session = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
     
     // Generate session signature
